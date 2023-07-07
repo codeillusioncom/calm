7776a72f4feb7117e794090ab33904e18f90c0d7
codemaker <codemaker@codeillusion.com>
2023-07-07 02:48:49 +0200

    View parameter fixes.

--- src/http/views/base_view.cr
+++ src/http/views/base_view.cr
@@ -1,6 +1,14 @@
 module Calm
   module Http
     class BaseView
+      macro use_var(*vars, &block)
+        {% for var, index in vars %}
+        {% if index % 2 == 0 %}
+        {{var.id}} : {{vars[index + 1]}} = vars["{{var.id}}"].get.as({{vars[index + 1]}})
+        {% end %}
+        {% end %}
+        {{block.body}}
+      end
     end
   end
 end
